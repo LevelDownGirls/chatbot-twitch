af1e6a7060526dbcbe783f49cb81bbde6b697361
leveldowngirls <leveldowngirls@gmail.com>
2017-10-31 11:35:13 +1100

    foo

---
+++ config.js
@@ -0,0 +1,12 @@
+module.exports = {
+        options: {password: "xxx"},
+        password: "xxx",
+        secure: "false",
+        sasl: false,
+        channels: ["#leveldowngirls"],
+        server: "irc.chat.twitch.tv",
+        botName: "LevelerBot",
+        userName: "LevelerBot",
+        showErrors: true,
+        floodProtection: true
+}
