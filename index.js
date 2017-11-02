d5f59f2ca028723a590fabfbf88907c9c3d58a25
leveldowngirls <leveldowngirls@gmail.com>
2017-11-02 14:37:11 +1100

    stylesheets and code cleanup

--- index.js
+++ index.js
@@ -6,31 +6,13 @@ var fs = require('fs');
 var path = require('path');
 var stdin = process.openStdin();
 var request = require('request');
-var app = require('express')();
+var express = require('express');
+var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
-
-
-// config
-var chatMode = 1;
-var production = 0;
-console.log("\n -- LIVE ON "+(production ? "TWITCH" : "TEST")+"! --");
-
-if(production){
-    var config = require('./config.js');
-}else{
-    var config = {
-        password: "",
-        channels: ["#foo"],
-        server: "127.0.0.1",
-        botName: "LevelerBot",
-        floodProtection: false
-    };
-}
-
-//startup text
-function startup(){
+// startup - write startup text
+function startup() {
     consoleOutput("\n\n");
     consoleOutput("Console commands:");
     consoleOutput(" !chat       - toggles chat commands on or off");
@@ -41,12 +23,187 @@ function startup(){
     consoleOutput("\n\nDon't forget to start the notifier or the bot will crash!");
 }
 
+function whisper(text, name) {
+    if (production) {
+        bot.say(config.channels[0], "/w " + name + " " + text);
+    } else {
+        bot.say(config.channels[0], "/msg " + name + " " + text);
+    }
+}
+
+// card posting
+function cardDeets(res, details, from) {
+    if (details) {
+        whisper(res.name + " - " + res.manaCost, from);
+        whisper(res.text, from);
+        whisper("http://gatherer.wizards.com/Pages/Card/Details.aspx?multiverseid=" + res.multiverseid, from);
+    } else {
+        bot.say(config.channels[0], description + res.name + " - " + res.manaCost);
+        bot.say(config.channels[0], "http://gatherer.wizards.com/Pages/Card/Details.aspx?multiverseid=" + res.multiverseid);
+    }
+}
+
+function hasFlag(flag, flagList) {
+    return (flagList.indexOf(flag) > -1);
+}
+
+// getCommand - understand input strings
+function getCommand(string) {
+    var spacePlace = string.indexOf(" ");
+    var flags = "";
+    if (spacePlace != -1) {
+        var before = string.substring(0, spacePlace);
+        var after = string.substring(spacePlace + 1);
+        if (after.startsWith("-")) {
+            // if text after flag, remove flag from text
+            if (after.indexOf(" ") != -1) {
+                flags = after.substring(0, after.indexOf(" "));
+                after = after.substring(after.indexOf(" ") + 1);
+            } else { //else remove text
+                flags = after;
+                after = "";
+            }
+            flags = flags.split(''); //seperate chars to array
+            flags.shift(); //remove the -
+        }
+    } else {
+        flags = [];
+        var before = string;
+        var after = "";
+    }
+    return {
+        command: before,
+        text: after,
+        flags: flags
+    };
+}
+
+//consoleOutput - send to console and browser
+function consoleOutput(text, nochat, noconsole) {
+    if (text.startsWith("/me")) {
+        text = text.substring(4);
+    }
+    io.emit('chat message', text);
+    console.log(text);
+}
+
+// check followers
+function checkFollowers(first) {
+    request(options, function(error, response, body) {
+        followers = JSON.parse(body);
+        var latest = followers["follows"][0]["user"]["display_name"];
+        if (latest != latestFollower) {
+            latestFollower = latest;
+            if (!first) {
+                newFollower(latest);
+            }
+        }
+    });
+
+    function callback() {
+
+    }
+}
+
+function newFollower(name) {
+    bot.say(config.channels[0], "/me " + name + " is now following LevelDownGirls");
+    request
+        .get("http://localhost:8888/?follow=" + name)
+        .on('error', function(err) {
+            console.log("Error sending follow notification for " + name + ". Did you start the notifier?");
+        })
+}
+
+function consoleSay(inp, send) {
+    if (inp.startsWith("!")) {
+        var input = getCommand(inp);
+        switch (input['command']) {
+            case "!chat":
+                chatMode = chatMode ? 0 : 1;
+                var msg = "/me -- CHAT MODE IS " + (chatMode ? "ON" : "OFF") + " --";
+                consoleOutput(msg);
+                bot.say(config.channels[0], msg);
+                break;
+            case "!lastfollow":
+                if (!hasFlag("q", input['flags']))
+                    bot.say(config.channels[0], "\/me " + latestFollower + " is now following LevelDownGirls");
+                consoleOutput(latestFollower + " is the latest follower");
+                break;
+            case "!spoof":
+                consoleOutput("Spoofing " + input["text"]);
+                newFollower(input["text"]);
+                break;
+            case "!help":
+                startup();
+                break;
+            case "!flood":
+                bot.activateFloodProtection(input["text"]);
+                consoleOutput(" -- FLOOD PROTECTION SET TO " + input["text"] + " --");
+                break;
+            case "!quit":
+                if (hasFlag("y", input["flags"]))
+                    process.exit();
+                quit = 1;
+                consoleOutput("ARE YOU SURE YOU WANT TO QUIT? Y/N");
+                break;
+            default:
+                consoleOutput("malformed command");
+        }
+    } else {
+        if (quit) {
+            if (inp == "y") {
+                consoleOutput("Quitting");
+                process.exit();
+            } else {
+                consoleOutput("Not quitting");
+                quit = 0;
+            }
+        } else {
+            //output to console
+            bot.say(config.channels[0], inp);
+        }
+    }
+}
+
+app.use(express.static(__dirname + '/public'));
+
+// config
+var chatMode = 1;
+var production = 0;
+var quit = 0;
+var path = "C:/Users/Princess Lexi-Lexi/Documents/Unp/";
+var music = path + "unp_now_playing.txt";
+var magic = path + "magic.txt";
+
+console.log("\n -- LIVE ON " + (production ? "TWITCH" : "TEST") + "! --");
+
+var config = require('./config.js');
+
+var options = {
+    url: 'https://api.twitch.tv/kraken/channels/leveldowngirls/follows?&limit=25&offset=0',
+    headers: {
+        'Client-ID': config['clientid']
+    }
+};
+
+if (!production) {
+    var config = {
+        password: "",
+        channels: ["#foo"],
+        server: "127.0.0.1",
+        botName: "LevelerBot",
+        floodProtection: false
+    };
+}
+
 //display console options
 startup();
 
 //init
 var bot = new irc.Client(config.server, config.botName, {
-    channels: config.channels, password:config.password, floodProtection: config.floodProtection
+    channels: config.channels,
+    password: config.password,
+    floodProtection: config.floodProtection
 });
 
 //verbose errors
@@ -60,81 +217,62 @@ bot.addListener('error', function(message) {
 
 });
 
-function whisper(text,name){
-    if(production){
-        bot.say(config.channels[0], "/w "+name+" "+text);
-    }else{
-        bot.say(config.channels[0], "/msg "+name+" "+text);
-    }
-}
 // Listen for any message said to them in the room
 bot.addListener("message", function(from, to, text, message) {
-    if(chatMode){
-        if(text.toLowerCase().startsWith("!time")){
-            var dt = dateTime.create();
-            var formatted = dt.format('w I:M:S p');
-            bot.say(config.channels[0], "It is "+formatted+" for the LevelDown Girls (AEST)");
-        }
-        if(text.toLowerCase().startsWith("!help")){
-            whisper("Hi! I am LevelerBot - a bot made by Lexi!",from);
-            whisper("I'm still under development but i hope you find me useful!",from);
-            whisper("I display card info, mention when theres a new follower, and tell you what music is playing",from);
-            whisper("My commands are:",from);
-            whisper("  !time - tells you what time it is for the LevelDown Girls",from);
-            whisper("  !Card [card name] - gives you info and a gatherer link",from);
-            whisper("If you have any suggestions for things i could learn to do, please tell lexi",from);
-            bot.say(config.channels[0], "/w "+from+" \n\n\n\n \n \n ");
-        }
-        if(text.toLowerCase().startsWith("!card ")){
-            var query = text.toLowerCase().substring(9); //remove command
-            query = query.replace(/^\s+|\s+$/g, ""); //trim whitespace
-            Magic.Cards.where({name: query}).then(results => {
-                cardDeets(results[0],1,from);
-            });
-        }
-        // SAY MY NAME
-        if(text.indexOf(config.botName) !== -1){
-            whisper("Hi there, "+from+". Type !help for help.",from);
+    if (chatMode) {
+        if (text.startsWith("!")) {
+            var input = getCommand(text);
+            switch(input['command']){
+                case "!time":
+                    var dt = dateTime.create();
+                    var formatted = dt.format('w I:M:S p');
+                    bot.say(config.channels[0], "It is " + formatted + " for the LevelDown Girls (AEST)");
+                    break;
+                case "!help":
+                    whisper("Hi! I am LevelerBot - a bot made by Lexi!", from);
+                    whisper("I'm still under development but i hope you find me useful!", from);
+                    whisper("I display card info, mention when theres a new follower, and tell you what music is playing", from);
+                    whisper("My commands are:", from);
+                    whisper("  !time - tells you what time it is for the LevelDown Girls", from);
+                    whisper("  !Card [card name] - gives you info and a gatherer link", from);
+                    whisper("If you have any suggestions for things i could learn to do, please tell lexi", from);
+                    break;
+                case "!card":
+                    Magic.Cards.where({
+                        name: input['text']
+                    }).then(results => {
+                        cardDeets(results[0], 1, from);
+                    });
+                    break;
+            }
+            // SAY MY NAME
+            if (text.indexOf(config.botName) !== -1) {
+                whisper("Hi there, " + from + ". Type !help for help.", from);
+            }
         }
     }
 });
 
-var path = "C:/Users/Princess Lexi-Lexi/Documents/Unp/";
-var music = path+"unp_now_playing.txt";
-var magic = path+"magic.txt";
-
 // say the playing track in chat
 watch(music, function(filename) {
-    fs.readFile(music, 'utf8', function (err,data) {
+    fs.readFile(music, 'utf8', function(err, data) {
         if (err) {
             return console.log(err);
         }
-        bot.say(config.channels[0], "Now listening to: "+data);
+        bot.say(config.channels[0], "Now listening to: " + data);
     });
 });
 
 // Post the card reader card
 watch(magic, function(filename) {
-    fs.readFile(magic, 'utf8', function (err,data) {
+    fs.readFile(magic, 'utf8', function(err, data) {
         if (err) {
             return console.log(err);
         }
-        Magic.Cards.find(data).then(result => cardDeets(result,0,"Card: "));
+        Magic.Cards.find(data).then(result => cardDeets(result, 0, "Card: "));
     });
 
 });
-
-// card posting
-function cardDeets(res, details,  from){
-    if(details){
-        whisper(res.name +" - "+res.manaCost,from);
-        whisper(res.text,from);
-        whisper("http://gatherer.wizards.com/Pages/Card/Details.aspx?multiverseid="+res.multiverseid,from);
-    }else{
-        bot.say(config.channels[0], description+res.name +" - "+res.manaCost);
-        bot.say(config.channels[0], "http://gatherer.wizards.com/Pages/Card/Details.aspx?multiverseid="+res.multiverseid);
-    }
-}
 
 // Type into console.
 stdin.addListener("data", function(d) {
@@ -142,101 +280,34 @@ stdin.addListener("data", function(d) {
     consoleSay(inp);
 });
 
-
-function consoleSay(inp){
-    if(inp.toLowerCase().startsWith("!chat")){
-        chatMode = chatMode?0:1;
-        var msg = "/me -- CHAT MODE IS "+(chatMode?"ON":"OFF")+" --";
-        consoleOutput(msg);
-        bot.say(config.channels[0], msg);
-    }else if(inp.toLowerCase().startsWith("!lastfollow")){
-        if(!inp.toLowerCase().startsWith("!lastfollow -q"))
-            bot.say(config.channels[0], "\/me "+latestFollower+" is now following LevelDownGirls");
-//        console.log(latestFollower+" is the latest follower");
-        consoleOutput(latestFollower+" is the latest follower");
-    }else if(inp.toLowerCase().startsWith("!spoof ")){
-        inp = inp.substring(7);
-        consoleOutput("spoofing "+inp);
-//        console.log("spoofing "+inp);
-        newFollower(inp);
-    }else if(inp.toLowerCase().startsWith("!help")){
-        startup();
-    }else if(inp.toLowerCase().startsWith("!flood ")){
-        inp = inp.toLowerCase().substring(7); //remove command
-        inp = inp.replace(/^\s+|\s+$/g, ""); //trim whitespace
-        bot.activateFloodProtection(inp);
-//        console.log( " -- FLOOD PROTECTION SET TO "+inp+" --" );
-        consoleOutput( " -- FLOOD PROTECTION SET TO "+inp+" --" );
-    }else if(inp.startsWith("!")){
-        //dont print malformed commands
-    }else{
-        // do print everything else
-        bot.say(config.channels[0], inp);
-    }
-}
-
-function consoleOutput(text){
-    if(text.startsWith("/me")){
-        text = text.substring(4); //remove command
-        //text = "<b>"+text+"</b>";
-    }
-    io.emit('chat message', text);
-    console.log(text);
-}
-
-
-var options = {
-    url: 'https://api.twitch.tv/kraken/channels/leveldowngirls/follows?&limit=25&offset=0',
-    headers: {
-        'Client-ID': 'fjuqde8drx14ae30yyr3g4stvm0ekz'
-    }
-};
-
 var latestFollower = "";
-function checkFollowers(first){
-    request(options, function (error, response, body) {
-        followers = JSON.parse(body);
-        var latest = followers["follows"][0]["user"]["display_name"];
-        if(latest != latestFollower){
-            latestFollower = latest;
-            if(!first){
-                newFollower(latest);
-            }
-        }
-    });
-    function callback(){
-
-    }
-}
-
-function newFollower(name){
-    bot.say(config.channels[0], "/me "+name+" is now following LevelDownGirls");
-    request
-        .get("http://localhost:8888/?follow="+name)
-        .on('error', function(err){
-            console.log("Error sending follow notification for "+name+". Did you start the notifier?");
-        })
-}
 
 checkFollowers(1);
-setInterval(function(){checkFollowers(0)}, 60*1000);
+setInterval(function() {
+    checkFollowers(0)
+}, 60 * 1000);
 
 // save follower info to a file to call back from OBS viewer thing
 
-app.get('/', function(req, res){
+app.get('/', function(req, res) {
     res.sendFile(__dirname + '/index.html');
 
 });
 
-
-io.on('connection', function(socket){
-    socket.on('chat message', function(msg){
-        io.emit('chat message', msg);
-//        console.log( msg);
-        consoleSay(msg);
+io.on('connection', function(socket) {
+    consoleSay("!help");
+    socket.on('chat message', function(msg) {
+        var mess = {
+            msg: msg,
+            client: 1
+        };
+        io.emit('chat message', mess);
+        consoleSay(msg, 1);
     });
 });
 
-http.listen(8080, function(){
+http.listen(8080, function() {
     console.log('listening on *:8080');
 });
+
+
